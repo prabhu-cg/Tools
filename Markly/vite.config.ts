f0cb677d98078f6b07ab83b2a425cae33b615334
Prabhu Raja <prabhu_cg@proton.me>
2026-05-03 14:09:42 +0100

    Update vite.config.ts
    Repo name added to base

--- Markly/vite.config.ts
+++ Markly/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Tools',
   build: {
     target: 'es2022',
     outDir: 'dist',
